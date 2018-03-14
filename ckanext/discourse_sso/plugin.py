b098d0cbced98947aa93f7e98fd362a1eac64637
JD Bothma <jbothma@gmail.com>
2018-03-14 17:03:55 +0200

    Fix signature check

--- ckanext/discourse_sso/plugin.py
+++ ckanext/discourse_sso/plugin.py
@@ -1,6 +1,6 @@
 import logging
 import ckan.plugins as p
-from ckan.plugins.toolkit import c, redirect_to, request, config
+from ckan.plugins.toolkit import c, redirect_to, request, config, abort, _
 from base64 import b64decode
 from urlparse import parse_qs
 import hashlib
@@ -39,16 +39,26 @@ class SSOController(p.toolkit.BaseController):
                         action='login', came_from=request.url)
 
         if not signature_is_valid(request):
-            (401, _('Incorrect Discourse SSO Signature to CKAN - please contact us.'))
+            raise Exception('Incorrect Discourse SSO Signature to CKAN')
 
         redirect_to("https://discourse.vulekamali.gov.za")
 
 
 def signature_is_valid(request):
-    payload = b64decode(request.params.get('sso'))
+    payload_b64 = request.params.get('sso')
+    log.debug("Payload Base64-encoded %s", payload_b64)
+    payload = b64decode(payload_b64)
+    log.debug("Payload %s", payload)
     nonce = parse_qs(payload)['nonce'][0]
+    log.debug("Nonce %s", nonce)
     their_sig = request.params.get('sig')
-    hash = hmac.new(sso_secret, nonce, hashlib.sha256)
-    our_sig = hash.hexdigest()
+    log.debug("Their signature %r", their_sig)
 
-    return their_sig != our_sig
+    log_safer_secret = "%s...hidden...%s" % (sso_secret[:3], sso_secret[-3:])
+    log.debug("SSO Secret %s", log_safer_secret)
+
+    hash = hmac.new(sso_secret, payload_b64, hashlib.sha256)
+    our_sig = unicode(hash.hexdigest())
+    log.debug("Our signature %r", our_sig)
+
+    return hmac.compare_digest(their_sig, our_sig)
