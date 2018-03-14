9c455c43bd82c5632c90f0b91483ac4aa94f9a3c
JD Bothma <jbothma@gmail.com>
2018-03-14 13:18:34 +0200

    WIP serve a redirect at a realistic path

--- ckanext/discourse_sso/plugin.py
+++ ckanext/discourse_sso/plugin.py
@@ -1,10 +1,16 @@
 from flask import Blueprint
 import ckan.plugins as p
+import ckan.lib.helpers as h
+
+from ckan.common import _, c, request, response
 
 
-def custom_action():
-    print "DEADBEEF"
-    # ...
+def sso():
+    # if not c.user:
+    #     h.redirect_to(controller='user',
+    #                   action='login', came_from=request.url)
+
+    return h.redirect_to("https://discourse.vulekamali.gov.za")
 
 
 class DiscourseSSOPlugin(p.SingletonPlugin):
@@ -12,9 +18,9 @@ class DiscourseSSOPlugin(p.SingletonPlugin):
     p.implements(p.IBlueprint)
 
     def get_blueprint(self):
-        blueprint = Blueprint('foo', self.__module__)
+        blueprint = Blueprint('discourse-sso', self.__module__)
         rules = [
-            ('/foo', 'custom_action', custom_action),
+            ('/discourse/sso', 'sso', sso),
         ]
         for rule in rules:
             blueprint.add_url_rule(*rule)
