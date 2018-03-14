c176a84a102bf56441c9eb288ae5882a6f77f477
JD Bothma <jbothma@gmail.com>
2018-03-14 16:11:44 +0200

    Switch to Pylons because I can't get Flask to work

--- ckanext/discourse_sso/plugin.py
+++ ckanext/discourse_sso/plugin.py
@@ -1,32 +1,54 @@
 import logging
-from flask import Blueprint
 import ckan.plugins as p
-import ckan.lib.helpers as h
-from urllib import urlencode
-
-from ckan.common import _, c, request, response
+from ckan.plugins.toolkit import c, redirect_to, request, config
+from base64 import b64decode
+from urlparse import parse_qs
+import hashlib
+import hmac
+import os
 
 log = logging.getLogger(__name__)
 
-
-def sso():
-    if not c.user:
-        log.info(request.url)
-        return h.redirect_to('/user/login?' + urlencode({'came_from': request.url}))
-
-    return h.redirect_to("https://discourse.vulekamali.gov.za")
+# Environment variable takes priority
+sso_secret = config.get('discourse.sso.secret')
+sso_secret = os.environ.get('CKAN_DISCOURSE_SSO_SECRET', sso_secret)
+if sso_secret is None:
+    raise Exception("Config 'discourse.sso.secret' or environment variable "
+                    "CKAN_DISCOURSE_SSO_SECRET must be set")
 
 
 class DiscourseSSOPlugin(p.SingletonPlugin):
 
-    p.implements(p.IBlueprint)
+    p.implements(p.IRoutes, inherit=True)
 
-    def get_blueprint(self):
-        blueprint = Blueprint('discourse-sso', self.__module__)
-        rules = [
-            ('/discourse/sso', 'sso', sso),
-        ]
-        for rule in rules:
-            blueprint.add_url_rule(*rule)
+    def before_map(self, map_):
+        # New route to custom action
+        map_.connect(
+            '/discourse/sso',
+            controller='ckanext.discourse_sso.plugin:SSOController',
+            action='sso')
 
-        return blueprint
+        return map_
+
+
+class SSOController(p.toolkit.BaseController):
+
+    def sso(self):
+        if not c.user:
+            redirect_to(controller='user',
+                        action='login', came_from=request.url)
+
+        if not signature_is_valid(request):
+            (401, _('Incorrect Discourse SSO Signature to CKAN - please contact us.'))
+
+        redirect_to("https://discourse.vulekamali.gov.za")
+
+
+def signature_is_valid(request):
+    payload = b64decode(request.params.get('sso'))
+    nonce = parse_qs(payload)['nonce'][0]
+    their_sig = request.params.get('sig')
+    hash = hmac.new(sso_secret, nonce, hashlib.sha256)
+    our_sig = hash.hexdigest()
+
+    return their_sig != our_sig
